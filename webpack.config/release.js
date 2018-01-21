6ea387227ceddcc3cb5d2ff9573249ac7ea67201
zsk <zsk526@qq.com>
2018-01-22 00:36:22 +0800

    Add webpack release

---
+++ webpack.config/release.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+
+module.exports = {
+    entry: './src/scripts/mosaic.js',
+
+    output: {
+        filename: 'index.js',
+        path: path.resolve('./lib'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env'],
+                    }
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new UglifyJsPlugin()
+    ]
+}
